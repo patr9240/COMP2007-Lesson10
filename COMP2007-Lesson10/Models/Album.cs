1f5122d714d532b8c5e09816024a0940df78f2a5
patr9240 <patr9240@gmail.com>
2016-07-25 14:55:30 -0400

    Updated album and genre model classes

--- COMP2007-Lesson10/Models/Album.cs
+++ COMP2007-Lesson10/Models/Album.cs
@@ -23,9 +23,14 @@ namespace COMP2007_Lesson10.Models
         {
             this.Title = Title;
         }
-
-        public int AlbumID { get; set; }
+        
+        public int AlbumId { get; set; }
+        public int GenreId { get; set; }
+        public int ArtistId { get; set; }
         public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string AlbumArtUrl { get; set; }
         public Genre Genre { get; set; }
+        public Artist Artist { get; set; }
     }
 }
---
+++ COMP2007-Lesson10/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2007_Lesson10.Models
+{
+        public class Artist
+        {
+            public int ArtistID { get; set; }
+            public string Name { get; set; }
+        }
+}
