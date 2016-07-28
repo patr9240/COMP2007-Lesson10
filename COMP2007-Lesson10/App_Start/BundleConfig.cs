fd1b46d880542c9374a3519a280b0325a3c08832
patr9240 <patr9240@gmail.com>
2016-07-27 22:12:06 -0400

    Modify scaffolded index view

--- COMP2007-Lesson10/App_Start/BundleConfig.cs
+++ COMP2007-Lesson10/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace COMP2007_Lesson10
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
